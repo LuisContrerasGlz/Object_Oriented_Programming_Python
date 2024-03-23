e4f75413ac0c0ca49d6695f74e8476c10c3d6836
Luis Francisco Contreras Gonzalez <luis.fco.contreras@gmail.com>
2024-03-22 20:29:49 -0600

    progress

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 class Item:
     pay_rate = 0.8 # The Pay rate after 20% discount 
+    all = [] 
 
     def __init__(self, name: str, price: float, quantity = 0):
         # Run validations to the recieved arguments
@@ -10,14 +11,19 @@ class Item:
         self.name = name
         self.price = price
         self.quantity = quantity
+
+        # Actions to execute
+        Item.all.append(self)
     
     def calculate_total_price(self):
         return self.price * self.quantity
     
     def apply_discount(self):
         self.price = self.price * self.pay_rate
-    
 
+    def __repr__(self):
+        return f"Item('{self.name}', {self.price}, {self.quantity})"
+    
 item1 = Item("Phone", 100, 2)
 item2 = Item("Laptoo", 200, 2)
 
@@ -30,5 +36,7 @@ print(item1.price())
 item3 = Item("Cable", 10, 5)
 item4 = Item("Mouse", 50, 5)
 item5 = Item("Keyboard", 75, 5)
+
+print(Item.all)
 
 
